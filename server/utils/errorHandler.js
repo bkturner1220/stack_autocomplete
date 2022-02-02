dab729f1a3cf9dd876cf2b7cec86ee02fc4b04c7
Brian Turner <47509604+bkturner1220@users.noreply.github.com>
2022-02-02 10:59:41 -0600

    Delete server directory

--- server/utils/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (error) => {
-  if (error.name === 'CastError' && error.kind === 'ObjectId') {
-    return 'Malformatted ID.';
-  } else {
-    return error.message;
-  }
-};
-
-module.exports = errorHandler;
